50a994ef17ebb5a9e90ec0ffe8dfc872b0b9c2a7
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-07-11 18:28:15 +0530

    updating code

--- src/main.ts
+++ src/main.ts
@@ -6,33 +6,91 @@ import * as io from '@actions/io';
 import { FormatType, SecretParser } from 'actions-secret-parser';
 
 var azPath: string;
-var prefix = !!process.env.AZURE_HTTP_USER_AGENT ? `${process.env.AZURE_HTTP_USER_AGENT}` : "";
-var azPSHostEnv = !!process.env.AZUREPS_HOST_ENVIRONMENT ? `${process.env.AZUREPS_HOST_ENVIRONMENT}` : "";
+// var prefix = !!process.env.AZURE_HTTP_USER_AGENT ? `${process.env.AZURE_HTTP_USER_AGENT}` : "";
+// var azPSHostEnv = !!process.env.AZUREPS_HOST_ENVIRONMENT ? `${process.env.AZUREPS_HOST_ENVIRONMENT}` : "";
 var workspacePath = !!process.env.GITHUB_WORKSPACE ? `${process.env.GITHUB_WORKSPACE}`:"";
 async function main() {
     try {
         // Set user agent variable
-        var isAzCLISuccess = false;
+        var isArmDeploymentSuccess = false;
         let usrAgentRepo = crypto.createHash('sha256').update(`${process.env.GITHUB_REPOSITORY}`).digest('hex');
-        let actionName = 'AzureLogin';
-        let userAgentString = (!!prefix ? `${prefix}+` : '') + `GITHUBACTIONS/${actionName}@v1_${usrAgentRepo}`;
-        let azurePSHostEnv = (!!azPSHostEnv ? `${azPSHostEnv}+` : '') + `GITHUBACTIONS/${actionName}@v1_${usrAgentRepo}`;
-        core.exportVariable('AZURE_HTTP_USER_AGENT', userAgentString);
-        core.exportVariable('AZUREPS_HOST_ENVIRONMENT', azurePSHostEnv);
+        let actionName = 'ArmDeploy';
+        // let userAgentString = (!!prefix ? `${prefix}+` : '') + `GITHUBACTIONS/${actionName}@v1_${usrAgentRepo}`;
+        // let azurePSHostEnv = (!!azPSHostEnv ? `${azPSHostEnv}+` : '') + `GITHUBACTIONS/${actionName}@v1_${usrAgentRepo}`;
+        // core.exportVariable('AZURE_HTTP_USER_AGENT', userAgentString);
+        // core.exportVariable('AZUREPS_HOST_ENVIRONMENT', azurePSHostEnv);
+
+        // get the input params
         let resource_group = core.getInput('resource_group',{required:true})
+        let mode = core.getInput('mode',{required:false})
+        let name = core.getInput('name',{required:false})
+        let rollback_on_error = core.getInput('rollback_on_error',{required:false})
+        let template_file = core.getInput('template_file',{required:true})
+        let parameter_file = core.getInput('parameter_file',{required:false})
+        let parameters = core.getInput('parameters',{required:false})
+
+
         azPath = await io.which("az", true);
         await executeAzCliCommand("--version");
-        let template_path = `${workspacePath}` + "/.cloud/.azure/deploy.json";
-        let template_param_path = `${workspacePath}` + "/.cloud/.azure/deploy.params.json";
-        let command = "group deployment validate -g ashkuma_functionAppRsGroup --template-file " + `${template_path}` + " --parameters " + `${template_param_path}` + " -o json";
-        await executeAzCliCommand(`${command}`);
+
+        let validation_command = "deployment group validate " + "-g " + `${resource_group}`
+        let deployment_command = "deployment group create " + "-g " + `${resource_group}`
+        let command = getCommandToExecute();
+        
+        command = validation_command + command + " -o json"; 
+       
+        //let command = "deployment group validate -g ashkuma_functionAppRsGroup --template-file " + `${template_path}` + " --parameters " + `${template_param_path}` + " -o json";
+        var deploy_result = await executeAzCliCommand(`${command}`);
     
     } finally {
-        // Reset AZURE_HTTP_USER_AGENT
-        core.exportVariable('AZURE_HTTP_USER_AGENT', prefix);
-        core.exportVariable('AZUREPS_HOST_ENVIRONMENT', azPSHostEnv);
+        // // Reset AZURE_HTTP_USER_AGENT
+        // core.exportVariable('AZURE_HTTP_USER_AGENT', prefix);
+        // core.exportVariable('AZUREPS_HOST_ENVIRONMENT', azPSHostEnv);
     }
 }
+
+
+
+function getCommandToExecute(){
+    var command = " ";
+    
+    let mode = core.getInput('mode',{required:false})
+    let name = core.getInput('name',{required:false})
+    let rollback_on_error = core.getInput('rollback_on_error',{required:false})
+    let template_file = core.getInput('template_file',{required:true})
+    let parameter_file = core.getInput('parameter_file',{required:false})
+    let parameters = core.getInput('parameters',{required:false})
+
+    let template_path = `${workspacePath}` + `${template_file}`;
+
+    if (mode != undefined){
+        command = command + " --mode " + `${mode}`
+    }
+
+    if(name == undefined){
+        name = `${process.env.GITHUB_REPOSITORY}`
+    }
+
+    command = command + " --name " + `${name}`;
+
+    if(rollback_on_error != undefined){
+        command = command + " --rollback_on_error " + `${rollback_on_error}`;
+    }
+
+    command = command + " --template-file " + `${template_path}`;
+
+    if (parameter_file != undefined){
+        let template_param_path = `${workspacePath}` + `${parameter_file}`;
+        command = command +  " --parameters " + `${template_param_path}`;
+    }
+
+    if (parameters != undefined){
+        command = command + " --parameters " + `${parameters}`;
+    }
+
+    return command;
+}
+
 
 async function executeAzCliCommand(command: string, silent?: boolean) {
     try {
