9e00cb08c63e09224cd0c35aaeb15d115383a771
vivishno <64971172+vivishno@users.noreply.github.com>
2020-08-19 15:18:11 +0530

    Update main.test.ts

--- _tests_/main.test.ts
+++ _tests_/main.test.ts
@@ -1,1 +1,15 @@
 
+import * as core from '@actions/core';
+import * as crypto from "crypto";
+import * as exec from '@actions/exec';
+import * as io from '@actions/io';
+import { main } from '../../src/main.ts';
+describe('Testing initialize', () => {
+    let m: main;
+    test('TEST', async () => {
+        core.setOutput('scope', 'resource_group');
+        eSpy = jest.spyOn(m, 'main');
+        await m.main();
+        expect(eSpy).toHaveBeenCalled();
+    });
+});
