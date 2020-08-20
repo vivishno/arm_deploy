57bea0ba8345b09df25dc6657e3c451db54be647
vivishno <64971172+vivishno@users.noreply.github.com>
2020-08-20 16:06:08 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
   testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['**/_tests_/*.test.ts],
   testRunner: 'jest-circus/runner',
   transform: {
     '^.+\\.ts$': 'ts-jest'
